6b9e17fd2b079aadd74e4cd06418089b72c89003
Carlos H Lopez <CHLopez@boldt.com.ar>
2021-08-18 16:46:54 -0300

    verificacion de password con bcryp, creacion user_roles, roles

--- db/script-inicial-mysql.sql
+++ db/script-inicial-mysql.sql
@@ -1,11 +1,12 @@
 -- Volcando estructura de base de datos para digitalers
-CREATE DATABASE IF NOT EXISTS bootcamp2;
-USE bootcamp2;
+DROP DATABASE bootcamp_digitalers;
+CREATE DATABASE IF NOT EXISTS bootcamp_digitalers;
+USE bootcamp_delete;
 
 CREATE TABLE users (
 	id int primary key auto_increment NOT NULL,
 	username VARCHAR(50) NOT NULL,
-	password VARCHAR(50) NOT NULL
+	password VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS newsletter (
@@ -118,6 +119,16 @@ CREATE TABLE IF NOT EXISTS pagos_ordenes (
   monto float NOT NULL
 ) ;
 
+CREATE TABLE IF NOT EXISTS roles (
+	id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	role VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users_roles (
+	users_id INT NOT NULL,
+	roles_id INT NOT NULL
+);
+
 #constraints
 
 #ARTICULOS
@@ -143,18 +154,22 @@ ALTER TABLE MARCAS ADD CONSTRAINT UN_marcas_codigo UNIQUE INDEX (CODIGO);
 ALTER TABLE CATEGORIAS ADD CONSTRAINT UN_categorias_codigo UNIQUE INDEX (CODIGO);
 #CUPONES
 ALTER TABLE CUPONES ADD CONSTRAINT CK_cupones_descuento CHECK(DESCUENTO >= 0 AND DESCUENTO <=100);
-alter table CUPONES add CONSTRAINT UN_cupon_codigo unique (CODIGO);
 #USERS
 ALTER TABLE USERS ADD CONSTRAINT UN_users_username UNIQUE (username);
 #SOCIOS
 ALTER TABLE SOCIOS ADD CONSTRAINT FK_socios_users FOREIGN KEY (users_id) REFERENCES USERS(ID);
+#ROLES
+ALTER TABLE ROLES ADD CONSTRAINT UN_ROLE UNIQUE (ROLE);
+#USERS_ROLES
+ALTER TABLE users_roles ADD CONSTRAINT fk_users_roles_id FOREIGN KEY (users_id) REFERENCES users(id); 
+ALTER TABLE users_roles ADD CONSTRAINT fk_users_roles_r_id FOREIGN KEY (roles_id) REFERENCES roles(id);
 
 #datos iniciales de prueba
 #paises
 insert into PAISES (descripcion, descripcion_corta,habilitada) values('ARGENTINA','ARG',1);
 insert into PAISES (descripcion, descripcion_corta,habilitada) values('BRASIL','BRA',1);
 #users
-INSERT INTO USERS (username, password) values('eduit','eduit');
+INSERT INTO USERS (username, password) values('eduit','$2y$10$pz6nmql6Sz5kOcNuSZw/reJSu1ZG1QYS8TMMRnNNZ0dO4zPARYD9m');
 INSERT INTO SOCIOS (nombre,apellido,email,fecha_alta,users_id,direccion,paises_id) VALUES('carlos','lopez','email@email.com',CURDATE(),(SELECT id FROM users LIMIT 1), NULL,(SELECT id FROM paises LIMIT 1));
 
 #categorias
@@ -170,7 +185,9 @@ insert into ESTADOS_ORDENES (descripcion,descripcion_corta,estado_final) values(
 insert into ESTADOS_ORDENES (descripcion,descripcion_corta,estado_final) values('ESTADO FACTURADA','FACTURADA', 1);
 #medios pagos
 insert into MEDIOS_PAGOS (descripcion,descripcion_corta,codigo,habilitada) values('EFECTIVO CONTRAREEMBOLSO','EFE_REE','001',1);
-#cupones
-insert into CUPONES (nombre,codigo,fecha_vigencia_desde,fecha_vigencia_hasta, descuento) values('BIENVENIDA','001',curdate(), date_add(curdate(), interval 10 day),50);
 #incrementar una fecha 10 dias en mysql
 #select date_add(curdate(), interval 10 day)
+#roles
+INSERT INTO roles(role) VALUES('ADMIN');
+INSERT INTO roles(role) VALUES('USER');
+INSERT INTO users_roles(USERS_ID,ROLES_ID) VALUES(1,1);
